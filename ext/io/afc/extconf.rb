2171e1370d081c4e3b5965cc8c9562887eedb947
Toshiyuki Suzumura <suz.labo@amail.plala.or.jp>
2013-06-25 18:21:17 +0900

    Add a dependency of libimobiledevice to LDFLAGS and CPPFLAGS.

--- ext/io/afc/extconf.rb
+++ ext/io/afc/extconf.rb
@@ -1,5 +1,7 @@
 require 'mkmf'
 
+$CPPFLAGS="`pkg-config --cflags libimobiledevice-1.0`"
+$LDFLAGS="`pkg-config --libs libimobiledevice-1.0`"
 pkg_config("libimobiledevice-1.0")
 
 create_makefile("io/afc/afc_base")
